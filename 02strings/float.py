64d731a115a312a91ec159d2d3f8e20d9bab179c
Harsha Vardhan <kpharsha@yahoo.com>
2024-02-18 15:34:53 +0530

    add main float

---
+++ 02strings/float.py
@@ -0,0 +1,25 @@
+# float variables
+num1 = 5.0
+num2 = 3.0
+
+# basic arthemetic
+result = num1+num2
+print("Addition: ",result)
+
+result2= num1-num2
+print("Subtraction: ",result2)
+
+result3 = num1*num2
+print("Multiplication: ",result3)
+
+result4 = num1/num2
+print("Divison: ",result4)
+
+result5 = num1 % num2
+print("Modulus: ",result5)
+print("Modulus: ",int(result5))
+
+
+# Rounding
+result6 =round(3.17548293456, 3) #round to 3 decimal places
+print("Rounded: ",result6)
---
+++ 02strings/main.py
@@ -0,0 +1,58 @@
+arn = "arn:aws:iam::123456789012:user/johndoe"
+
+print(arn.split("/"))
+
+str1 = "Hello"
+str2 = "World"
+
+result = str1+" "+str2
+print(result)
+
+text = "I lead the division that plans and executes the production of made-to-fit clothing for men, by tailoring unit aggregation to fabric and other vendor partners. Skills: Operations Management · Vendor Management · Sales · Marketing · Supply chain"
+length = len(text)
+
+print("Length of the string:",length)
+
+# string - upper & lower case
+text1 = "python is awesome"
+uppercase = text1.upper()
+lowercase = text1.lower()
+print("Uppercase: ",uppercase)
+print("lowercase: ",lowercase)
+
+#  replace text using string replace
+new_text1 = text1.replace("awesome","great")
+print("Modified text: ",new_text1)
+
+# string split 
+text2 = "python is awesome"
+words = text2.split()
+print("Words:",words)
+
+# string-strip
+text3  = "     Some spaces around     "
+stripped_text = text3.strip()
+print("Stripped text: ",stripped_text)
+
+text4 = "python is awesome"
+substring = "is"
+if substring in text4:
+    print(substring,"found in the text")
+
+# integer variables
+num1 = 10
+num2 = 5
+
+# integer division
+result1 = num1//num2
+print("Interger division: ",result1)
+
+# Modulus (Remainder)
+result2 = num1 % num2
+print("modulus remainder : ",result2)
+
+# Absoluete value
+result3 = abs(-7)
+print("absolute value : ",result3)
+
+# 
