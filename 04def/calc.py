109e207c5e18906d3b269f6e63a20615ffe0f195
Harsha Vardhan <kpharsha@yahoo.com>
2024-02-23 20:58:01 +0530

    add

---
+++ 04def/calc.py
@@ -0,0 +1,12 @@
+def addition(num1,num2):
+    add = num1+num2
+    return add  
+
+def sub(num1,num2):
+    s = num1 - num2
+    return s
+
+
+def mul(num1,num2):
+    m = num1-num2
+    return m
---
+++ 04def/calc2.py
@@ -0,0 +1,3 @@
+import calc as calc
+
+print(calc.addition(10,8))
