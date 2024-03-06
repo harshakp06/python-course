b448d379d75fa5cf81b408b68aa78530ecffa906
Harsha Vardhan <kpharsha@yahoo.com>
2024-03-06 19:33:54 +0530

    add

--- 04def/os.py
+++ 04def/os.py
@@ -1,5 +1,8 @@
 import os
 
+# for printing and using environment variables 
+# it will be mostly used for sensitive information to use in the program file
+
 print(os.getenv("HOME"))
 
 print(os.getenv("SHELL"))
