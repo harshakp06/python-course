cbe06219b8ba5cdd9e695c92c9b3f319c2139b21
Harsha Vardhan <kpharsha@yahoo.com>
2024-03-06 20:54:18 +0530

    add

---
+++ 06operators/main.py
@@ -0,0 +1,20 @@
+a = 6
+b = 7
+
+print("Comparison operators")
+print(a>b)
+print(a<b)
+print(a==b)
+print(a>=b)
+print(a<=b,end="\n\n") #two new lines
+
+x = True
+y = False
+print("logical operators")
+print(x and y)
+print(x or y)
+print(not y)
+
+
+
+
