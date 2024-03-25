95c87fb064864f6cc27128fd90bf10150bf466ed
Harsha Vardhan <kpharsha@yahoo.com>
2024-03-25 19:14:23 +0530

    07 sol

---
+++ 15condi/07_solution.py
@@ -0,0 +1,10 @@
+order_size = "Medium"
+
+extra_shot = True
+
+if extra_shot:
+    coffee = order_size + " coofee with an extra shot"
+else:
+    coffee = order_size + " Coffee"
+
+print("order: ",coffee)
