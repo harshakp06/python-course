addbf93d89cf8ef0178833e6d74fa794439695bd
Harsha Vardhan <kpharsha@yahoo.com>
2024-03-28 19:09:10 +0530

    add 02 sol py

--- 18oopssol/02_sol.py
+++ 18oopssol/02_sol.py
@@ -3,10 +3,16 @@ class Car:
         self.mybrand = brand
         self.mymodel = model
 
+    # 2nd sol
     
+    def full_name(self):
+        # return self.mybrand + " "+ self.mymodel
+        return f"{self.mybrand}  {self.mymodel}"
+
+
 
 my_car = Car("Toyota","Corolla")
 print(my_car)
 print(my_car.mybrand)
 print(my_car.mymodel)
-
+print(my_car.full_name())
