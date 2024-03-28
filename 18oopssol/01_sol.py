8b9f7913a044360f2114a3efca542a97ffa799c6
Harsha Vardhan <kpharsha@yahoo.com>
2024-03-28 16:09:10 +0530

    add sol 2

---
+++ 18oopssol/01_sol.py
@@ -0,0 +1,12 @@
+class Car:
+    def __init__(self,brand,model):
+        self.mybrand = brand
+        self.mymodel = model
+
+
+
+my_car = Car("Toyota","Corolla")
+print(my_car)
+print(my_car.mybrand)
+print(my_car.mymodel)
+
